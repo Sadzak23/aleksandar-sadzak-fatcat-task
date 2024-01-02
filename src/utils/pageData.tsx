619dd3ab8827e38fec291924d0ec2b36a8937b81
Sadzak23 <sadzak23@gmail.com>
2024-01-02 22:15:00 +0100

    Move assets to the src folder

--- src/utils/pageData.tsx
+++ src/utils/pageData.tsx
@@ -1,45 +1,18 @@
+import cat from '@homework-task/assets/cats/cat_5.png';
+import cat2 from '@homework-task/assets/cats/cat_6.png';
+import cat3 from '@homework-task/assets/cats/cat_7.png';
+import cat4 from '@homework-task/assets/cats/cat_8.png';
+import cat5 from '@homework-task/assets/cats/cat_9.png';
 import { IPageGeneratorDataItem } from '@homework-task/types/IGeneratorData';
-
-import cat from '../../public/media/cats/cat_5.png';
-import cat2 from '../../public/media/cats/cat_6.png';
-import cat3 from '../../public/media/cats/cat_7.png';
-import cat4 from '../../public/media/cats/cat_8.png';
-import cat5 from '../../public/media/cats/cat_9.png';
 
 export const homePageData: IPageGeneratorDataItem[] = [
     {
         id: 'users',
-        layout: {
-            type: 'column',
-        },
+        layout: { type: 'column' },
         components: [{ id: 'usersList', type: 'usersList' }],
     },
     {
-        id: 'section2',
-        layout: {
-            type: 'column',
-            props: { background: 'bg-yellow' },
-        },
-        components: [
-            {
-                id: 'catHero',
-                type: 'hero',
-                props: {
-                    title: 'Hungry?',
-                    image: cat,
-                },
-            },
-            {
-                id: 'catSlide',
-                type: 'trustBar',
-                props: {
-                    images: [cat, cat2, cat3, cat4, cat5],
-                },
-            },
-        ],
-    },
-    {
-        id: 'section3',
+        id: 'items',
         layout: {
             type: 'row',
             props: { background: 'bg-red' },
@@ -96,6 +69,30 @@ export const homePageData: IPageGeneratorDataItem[] = [
                             onClick: () => {},
                         },
                     ],
+                },
+            },
+        ],
+    },
+    {
+        id: 'catsShow',
+        layout: {
+            type: 'column',
+            props: { background: 'bg-yellow' },
+        },
+        components: [
+            {
+                id: 'catHero',
+                type: 'hero',
+                props: {
+                    title: 'Hungry?',
+                    image: cat,
+                },
+            },
+            {
+                id: 'catSlide',
+                type: 'trustBar',
+                props: {
+                    images: [cat, cat2, cat3, cat4, cat5],
                 },
             },
         ],
